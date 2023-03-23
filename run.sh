c68c948ecde668a5b3e6424e6fd79b546fc416a3
pcl <598369987@qq.com>
2023-03-23 22:37:06 +0800

    xxx

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #log stream --process mycmd --style syslog | grep "com.gg.mycmd.log:"
 #xcode-select --switch /Applications/Xcode.app/Contents/Developer
 #xcodebuild -showsdks
+#xcrun simctl boot "iPhone 12 Pro Max"
 xcrun simctl uninstall booted com.gg.mycmd
 xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sdk iphonesimulator16.2 -target mycmd
 xcrun simctl install booted build/Release-iphonesimulator/mycmd.app
