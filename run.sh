8033b142e1b1255fc7f61fc6c2f0ff9d5ded336d
pcl <598369987@qq.com>
2023-03-26 19:42:16 +0800

    xxx

--- run.sh
+++ run.sh
@@ -6,3 +6,8 @@ xcrun simctl uninstall booted com.gg.mycmd
 xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -arch x86_64 -sdk iphonesimulator16.2 -target mycmd
 xcrun simctl install booted build/Release-iphonesimulator/mycmd.app
 xcrun simctl launch booted com.gg.mycmd
+
+#archive .app to ipa
+#cp -r build/Release-iphoneos/mycmd.app Payload
+#zip -r mycmd.zip Payload
+#mv mycmd.zip mycmd.ipa
